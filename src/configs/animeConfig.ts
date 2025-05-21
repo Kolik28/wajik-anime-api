b37b402810f2cdd3e6b4d562189d6eeda0f99e4a
nodelik <161203867+Kolik28@users.noreply.github.com>
2025-05-21 12:38:38 +0800

    update domain

--- src/configs/animeConfig.ts
+++ src/configs/animeConfig.ts
@@ -3,7 +3,7 @@ const animeConfig = {
 
   baseUrl: {
     otakudesu: "https://otakudesu.cloud",
-    samehadaku: "https://samehadaku.today",
+    samehadaku: "https://samehadaku.forum",
   },
 
   response: {
